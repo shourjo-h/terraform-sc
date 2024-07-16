6cd86284fce74f9b69ce191897893bb6bf527eab
Shourjo <54590698+shourjo-h@users.noreply.github.com>
2024-07-16 12:31:19 +0530

    Update storage_user-data.tf

--- storage_user-data.tf
+++ storage_user-data.tf
@@ -1,6 +1,4 @@
-# export AWS_ACCESS_KEY_ID="access_key"
-# export AWS_SECRET_ACCESS_KEY="secret_key"
-# export AWS_REGION="region_code"
+# env-variable.tf
 
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_ssh_http"
